c8d635dc1895378a28d12fc1c9d102aec5194fc4
Princi Porwal <133786604+PrinciPorwal@users.noreply.github.com>
2025-04-01 02:03:36 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Resume_template (23).pdf";
+import pdf from "../../Assets/Academic_CV_Template.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
